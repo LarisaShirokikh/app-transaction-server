cd8893454edf202b6d8c40ae8f32a182d4f6660f
larisasirokih <Lorashirokikh@yandex.ru>
2024-05-05 21:48:20 +0300

    modifi main corse

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: [`http://localhost:3000`, `https://31.129.56.94`, `http://localhost:8080`],
-    credentials: true,
-    exposedHeaders: 'set-cookie',
-  });
+  app.enableCors()
+  //   origin: [
+  //     `http://localhost:3000`,
+  //     `https://31.129.56.94`,
+  //     `http://localhost:8080`,
+  //   ],
+  //   credentials: true,
+  //   exposedHeaders: 'set-cookie',
+  // });
   await app.listen(PORT);
   console.log(`Сервер запущен и работает на порту ${PORT}`);
 }
