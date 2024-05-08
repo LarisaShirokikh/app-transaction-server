7f9e0c753bf30efd81d05f0740c706ac2b74fd1e
larisasirokih <Lorashirokikh@yandex.ru>
2024-05-08 16:10:26 +0300

    modify cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import * as cookieParser from 'cookie-parser'
 const PORT = process.env.PORT || 4200
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, {
-		cors: true
-	})
+	const app = await NestFactory.create(AppModule)
 
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
@@ -20,19 +18,12 @@ async function bootstrap() {
 			'http://localhost:3000',
 			'http://localhost:8080'
 		],
-		allowedHeaders: [
-			'Access-Control-Allow-Origin',
-			'Access-Control-Allow-Headers',
-			'Authorization',
-			'X-Requested-With',
-			'X-HTTP-Method-Override',
-			'Content-Type',
-			'Accept',
-			'Observe'
-		],
+		allowedHeaders: 'Content-Type, Authorization',
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
 		credentials: true,
-		exposedHeaders: 'set-cookie'
+		exposedHeaders: ['set-cookie'], 
+		preflightContinue: false, 
+		optionsSuccessStatus: 204 
 	})
 	await app.listen(PORT)
 	console.log(`Сервер запущен и работает на порту ${PORT}`)
